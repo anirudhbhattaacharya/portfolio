b54f207c53e2f76da09768c70c604c7dbb648064
Anirudh Bhattacharya <anirudhbhattaacharya@gmail.com>
2026-09-05 21:23:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector(".menu-btn");
+const nav = document.querySelector(".nav nav");
+if (btn && nav) {
+  btn.addEventListener("click", () => nav.classList.toggle("open"));
+  nav.querySelectorAll("a").forEach((a) =>
+    a.addEventListener("click", () => nav.classList.remove("open"))
+  );
+}
